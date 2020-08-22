598adbac636af53cb0192c1b144c432a4feae393
David Andrews <dandrewsuk82@gmail.com>
2020-08-22 17:37:40 +0100

    Autogit: 22-08-2020 17:37

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -117,6 +117,7 @@ const Contact = () => {
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="form-name" value="contact" />
             <TextField
               variant="outlined"
               margin="dense"
