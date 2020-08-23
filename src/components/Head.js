163a5c0861ce546f95f126b45d647148e018656c
David Andrews <dandrewsuk82@gmail.com>
2020-08-23 14:18:53 +0100

    Autogit: 23-08-2020 14:18

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,12 +3,23 @@ import { Helmet } from "react-helmet"
 
 const Head = () => {
   return (
-    <Helmet>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins&family=Montserrat&display=swap"
-        rel="stylesheet"
-      />
-    </Helmet>
+    <>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <form
+        name="contact"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        method="post"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+      </form>
+    </>
   )
 }
 
